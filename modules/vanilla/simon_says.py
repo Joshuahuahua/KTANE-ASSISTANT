5384eeeadb968df04140ccfa9c992a2a104ffef0
Nelio <46218259+ioNelio@users.noreply.github.com>
2020-06-11 00:03:22 +0800

    Started & finshed simon_says.py

---
+++ modules/vanilla/simon_says.py
@@ -0,0 +1,62 @@
+def simon_says(bomb_data):
+    
+    vowel = {
+    'red': ['blue', 'yellow', 'green'],
+    'blue': ['red', 'green', 'red'],
+    'green': ['yellow', 'blue', 'yellow'],
+    'yellow': ['green', 'red', 'blue']
+    }
+
+    non_vowel = {
+    'red': ['blue', 'red', 'yellow'],
+    'blue': ['yellow', 'blue', 'green'],
+    'green': ['green', 'yellow', 'blue'],
+    'yellow': ['red', 'green', 'red']
+    }
+
+    memory = []
+
+    while True:
+        try:
+            strikes = int(input('Enter number of strikes (0, 1 or 2): ').lower())
+            if strikes > -1 and strikes < 3:
+                break
+            else:
+                continue
+        except ValueError:
+            pass
+
+    while True:
+        try:
+            user_input = input('Usage <color> / <(s) edit strikes> / <(e)xit>: ').lower()
+            if user_input == 'e' or user_input == 'exit':
+                break
+            elif user_input == 's' or user_input == 'strikes':
+                memory = []
+                while True:
+                        try:
+                            strikes = int(input('Enter number of strikes (0, 1 or 2): ').lower())
+                            if strikes > -1 and strikes < 3:
+                                break
+                            else:
+                                continue
+                        except ValueError:
+                            pass 
+            elif bomb_data['serial_vowel']:
+                memory.append(vowel[user_input][strikes])
+                print('------- INPUT IN FOLLOWING ORDER -------')
+                for color in memory:
+                    print(color)
+            else:
+                memory.append(non_vowel[user_input][strikes])
+                print('------- INPUT IN FOLLOWING ORDER -------')
+                for color in memory:
+                    print(color)
+        except KeyError:
+            pass
+
+bomb_data = {
+        'serial_vowel': False
+    }
+
+simon_says(bomb_data)
