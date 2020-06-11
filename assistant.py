6a6b19d0eae318c50423762a355216c5056de26a
Nelio <46218259+ioNelio@users.noreply.github.com>
2020-06-12 01:45:00 +0800

    Finished gathering bomb data

--- assistant.py
+++ assistant.py
@@ -8,6 +8,9 @@ from modules.vanilla.complex_wires import complex_wires
 from modules.vanilla.knob import knob
 from modules.vanilla.whos_on_first import whos_on_first
 
+ind_lit = []
+ind_unlit = []
+
 # List of unlit/lit (2 seperate lists) indicators. all caps
 # Serial number
 # Number of big batteries
@@ -32,18 +35,48 @@ while True:
         print('Please enter a whole number')
         pass
 
+while True:
+    user_input = input('Enter the labels of the lit indicators (Enter nothing to continue/skip): ')
+    if user_input == '':
+        break
+    else:
+        ind_lit.append()
+
+while True:
+    user_input = input('Enter the labels of the unlit indicators (Enter nothing to continue/skip): ')
+    if user_input == '':
+        break
+    else:
+        ind_unlit.append()
+
+serial = input('Serial number: ').upper()
+if (int(serial[-1]) % 2) == 0:
+    serial_odd = False
+else:
+    serial_odd = True
+
+vowels = set('aeiou')
+serial_vowel = False
+for vowel in vowels:
+    if vowel in serial:
+        serial_vowel = True
+
+if input('Is there a parallel port? (Y/N)').lower() == 'y':
+    port_parallel = True
+else:
+    port_parallel = False
 
 
 bomb_data = {
     'bat_AA': aabatteries,
     'bat_B': bigbatteries,
     'bat_total': aabatteries+bigbatteries,
-    'ind_LIT': [],
-    'ind_UNLIT': [],
-    'serial': input('Serial number:'),
-    'serial_odd': False,
-    'serial_vowel': False,
-    'port_parallel': False,
+    'ind_LIT': ind_lit,
+    'ind_UNLIT': ind_unlit,
+    'serial': serial,
+    'serial_odd': serial_odd,
+    'serial_vowel': serial_vowel,
+    'port_parallel': port_parallel,
 }
 
 
