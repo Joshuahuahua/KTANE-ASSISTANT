4ce18bf1e92934102988c7fd5a7af72128593721
Joshuahuahua <joshuahollander2001@gmail.com>
2020-06-12 20:39:17 +0800

    finished morse.py
    error-handled assistant.py and integrated morse.py

--- assistant.py
+++ assistant.py
@@ -7,18 +7,16 @@ from modules.vanilla.sequence import sequence
 from modules.vanilla.complex_wires import complex_wires
 from modules.vanilla.knob import knob
 from modules.vanilla.whos_on_first import whos_on_first
+from modules.vanilla.morse import morse
 
 ind_lit = []
 ind_unlit = []
 
-# List of unlit/lit (2 seperate lists) indicators. all caps
-# Serial number
-# Number of big batteries
-# Number of double A batteries
+print('---Setup Initializing---')
 while True:
     try:
-        aabatteries = int(input('Number of idividual AA batteries: '))
-        if (aabatteries % 2) == 0:
+        bat_aa = int(input('Number of idividual AA batteries: '))
+        if (bat_aa % 2) == 0:
             break
         else:
             print('Please enter an even number (AA Batteries always come in pairs)')
@@ -29,33 +27,37 @@ while True:
 
 while True:
     try:
-        bigbatteries = int(input('Number of big batteries: '))
+        bat_b = int(input('Number of big batteries: '))
         break
     except ValueError:
         print('Please enter a whole number')
         pass
 
 while True:
-    user_input = input('Enter the labels of the lit indicators (Enter nothing to continue/skip): ')
+    user_input = input('Enter the labels of the LIT indicators (Enter nothing to continue/skip): ')
     if user_input == '':
         break
     else:
         ind_lit.append()
 
 while True:
-    user_input = input('Enter the labels of the unlit indicators (Enter nothing to continue/skip): ')
+    user_input = input('Enter the labels of the UNLIT indicators (Enter nothing to continue/skip): ')
     if user_input == '':
         break
     else:
         ind_unlit.append()
 
-serial = input('Serial number: ').upper()
-if (int(serial[-1]) % 2) == 0:
-    serial_odd = False
-else:
-    serial_odd = True
+while True:
+    try:
+        serial = input('Serial number: ').upper()
+        serial_odd = True
+        if (int(serial[-1]) % 2) == 0:
+            serial_odd = False
+        break
+    except:
+        print('---ERROR---\nPlease input a valid serial number!')
 
-vowels = set('aeiou')
+vowels = set('AEIOU')
 serial_vowel = False
 for vowel in vowels:
     if vowel in serial:
@@ -66,11 +68,10 @@ if input('Is there a parallel port? (Y/N)').lower() == 'y':
 else:
     port_parallel = False
 
-
 bomb_data = {
-    'bat_AA': aabatteries,
-    'bat_B': bigbatteries,
-    'bat_total': aabatteries+bigbatteries,
+    'bat_AA': bat_aa,
+    'bat_B': bat_b,
+    'bat_total': bat_aa+bat_b,
     'ind_LIT': ind_lit,
     'ind_UNLIT': ind_unlit,
     'serial': serial,
@@ -79,12 +80,11 @@ bomb_data = {
     'port_parallel': port_parallel,
 }
 
-
+print(bomb_data)
 while True:
     print('\n------------------------------------------')
     print('Keep Talking and Nobody Explodes Assistant')
     print('------------------------------------------\n')
-
 
     user_input = input('Enter a module (type "help" for options) > ').lower()
     if user_input == 'simple wires' or user_input == 'simple_wires':
@@ -105,12 +105,14 @@ while True:
         knob()
     elif user_input == 'whos on first' or user_input == 'who\'s on first' or user_input == 'wof':
         whos_on_first()
+    elif user_input == 'morse' or user_input == 'morse code':
+        morse()
     
     elif user_input == 'help':
         print('\n------OPTIONS------')
         print('Simple Wires, Button, Symbols, Memory')
         print('Password, Sequence, Complex Wires, Knob')
-        print('Who\'s on first')
+        print('Who\'s on first, Morse code')
         print('-------------------')
     elif user_input == 'quit':
         print('Goodbye!')
