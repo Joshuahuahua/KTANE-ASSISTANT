270b1c37f17021af0697c5c8f30c1c3422bbcc99
Joshuahuahua <joshuahollander2001@gmail.com>
2020-06-24 15:01:38 +0800

    Completed maze.py

--- assistant.py
+++ assistant.py
@@ -1,6 +1,7 @@
 from modules.vanilla.simple_wires import simple_wires
 from modules.vanilla.button import button
 from modules.vanilla.symbols import symbols
+from modules.vanilla.simon_says import simon_says
 from modules.vanilla.memory import memory
 from modules.vanilla.password import password
 from modules.vanilla.sequence import sequence
@@ -10,78 +11,81 @@ from modules.vanilla.whos_on_first import whos_on_first
 from modules.vanilla.morse import morse
 from modules.vanilla.maze import maze
 
-ind_lit = []
-ind_unlit = []
 
-print('---Setup Initializing---')
-while True:
-    try:
-        bat_aa = int(input('Number of idividual AA batteries: '))
-        if (bat_aa % 2) == 0:
+def init():
+    ind_lit = []
+    ind_unlit = []
+    print('---Setup Initializing---')
+    while True:
+        try:
+            bat_aa = int(input('Number of idividual AA batteries: '))
+            if (bat_aa % 2) == 0:
+                break
+            else:
+                print('Please enter an even number (AA Batteries always come in pairs)')
+                continue
+        except ValueError:
+            print('Please enter a whole number')
+            pass
+
+    while True:
+        try:
+            bat_b = int(input('Number of big batteries: '))
+            break
+        except ValueError:
+            print('Please enter a whole number')
+            pass
+
+    while True:
+        user_input = input('Enter the labels of the LIT indicators (Enter nothing to continue/skip): ')
+        if user_input == '':
             break
         else:
-            print('Please enter an even number (AA Batteries always come in pairs)')
-            continue
-    except ValueError:
-        print('Please enter a whole number')
-        pass
+            ind_lit.append(user_input)
 
-while True:
-    try:
-        bat_b = int(input('Number of big batteries: '))
-        break
-    except ValueError:
-        print('Please enter a whole number')
-        pass
+    while True:
+        user_input = input('Enter the labels of the UNLIT indicators (Enter nothing to continue/skip): ')
+        if user_input == '':
+            break
+        else:
+            ind_unlit.append(user_input)
 
-while True:
-    user_input = input('Enter the labels of the LIT indicators (Enter nothing to continue/skip): ')
-    if user_input == '':
-        break
+    while True:
+        try:
+            serial = input('Serial number: ').upper()
+            serial_odd = True
+            if (int(serial[-1]) % 2) == 0:
+                serial_odd = False
+            break
+        except:
+            print('---ERROR---\nPlease input a valid serial number!')
+
+    vowels = set('AEIOU')
+    serial_vowel = False
+    for vowel in vowels:
+        if vowel in serial:
+            serial_vowel = True
+
+    if input('Is there a parallel port? (Y/N): ').lower() == 'y':
+        port_parallel = True
     else:
-        ind_lit.append(user_input)
+        port_parallel = False
 
-while True:
-    user_input = input('Enter the labels of the UNLIT indicators (Enter nothing to continue/skip): ')
-    if user_input == '':
-        break
-    else:
-        ind_unlit.append(user_input)
+    bomb_data = {
+        'bat_AA': bat_aa,
+        'bat_B': bat_b,
+        'bat_total': bat_aa+bat_b,
+        'ind_LIT': ind_lit,
+        'ind_UNLIT': ind_unlit,
+        'serial': serial,
+        'serial_odd': serial_odd,
+        'serial_vowel': serial_vowel,
+        'port_parallel': port_parallel,
+    }
+    return bomb_data
 
-while True:
-    try:
-        serial = input('Serial number: ').upper()
-        serial_odd = True
-        if (int(serial[-1]) % 2) == 0:
-            serial_odd = False
-        break
-    except:
-        print('---ERROR---\nPlease input a valid serial number!')
+bomb_data = init()
 
-vowels = set('AEIOU')
-serial_vowel = False
-for vowel in vowels:
-    if vowel in serial:
-        serial_vowel = True
-
-if input('Is there a parallel port? (Y/N)').lower() == 'y':
-    port_parallel = True
-else:
-    port_parallel = False
-
-bomb_data = {
-    'bat_AA': bat_aa,
-    'bat_B': bat_b,
-    'bat_total': bat_aa+bat_b,
-    'ind_LIT': ind_lit,
-    'ind_UNLIT': ind_unlit,
-    'serial': serial,
-    'serial_odd': serial_odd,
-    'serial_vowel': serial_vowel,
-    'port_parallel': port_parallel,
-}
-
-print(bomb_data)
 while True:
     print('\n------------------------------------------')
     print('Keep Talking and Nobody Explodes Assistant')
@@ -94,6 +98,8 @@ while True:
         button(bomb_data)
     elif user_input == 'symbols':
         symbols()
+    elif user_input == 'simon says':
+        simon_says(bomb_data)
     elif user_input == 'memory':
         memory()
     elif user_input == 'password':
