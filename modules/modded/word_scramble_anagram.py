ef63e244e1c3ceb931d585e5f51a18a789fd912d
Joshuahuahua <joshuahollander2001@gmail.com>
2020-06-26 03:40:33 +0800

    Update word_scramble_anagram.py
    Renamed file
    Renamed and updated dictionary reference directory
    Forced input .upper()
    Stopped function from returning the input word.
    Added error handle

--- modules/modded/word_scramble_anagram.py
+++ modules/modded/word_scramble_anagram.py
@@ -2,14 +2,17 @@
 import json
 from itertools import permutations 
 
-def word_scramble(scramble):
-    with open('dependables\six_word.json', 'r') as dump:
+def word_scramble_anagram():
+    scramble = input('Input 6-digit string: ').upper()
+    with open('modules\modded\dependables\word_scramble_anagram.json', 'r') as dump:
         words = json.load(dump)
     scrambled = [''.join(p) for p in permutations(scramble)]
     #remove duplicates by making list items as keys and change it back to a list. 
     scrambled = list(dict.fromkeys(scrambled))
     for y in scrambled:
         for x in words:
-            if y == x:
-                print('the word is:', x)
-                return x
+            if y == x and y != scramble: # Stop program returning the same word
+                print('\nThe word is:', x)
+                return
+    print('---ERROR---\nCould not find valid word!')
+    return
