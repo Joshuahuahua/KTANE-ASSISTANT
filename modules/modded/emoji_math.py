29c05eadfb4e5e9da158b6e524ef904f888e5669
Joshuahuahua <jhollan9@our.ecu.edu.au>
2020-06-27 18:41:43 +0800

    Added error handling

--- modules/modded/emoji_math.py
+++ modules/modded/emoji_math.py
@@ -13,11 +13,16 @@ def emoji_math():
     {'symbol': '|:', 'value': '9'}]
 
     expression = input('Input the expression on the bomb: ')
-    for term in emoji_dic:
-        expression = expression.replace(term['symbol'], term['value'])
+    try:
+        for term in emoji_dic:
+            expression = expression.replace(term['symbol'], term['value'])
 
-    if expression.count('+') == 1:
-        result = int(expression.split('+')[0]) + int(expression.split('+')[1])
-    elif expression.count('-') == 1:
-        result = int(expression.split('-')[0]) - int(expression.split('-')[1])
-    print('The answer is:', result)
+        if expression.count('+') == 1:
+            result = int(expression.split('+')[0]) + int(expression.split('+')[1])
+        elif expression.count('-') == 1:
+            result = int(expression.split('-')[0]) - int(expression.split('-')[1])
+        print('The answer is:', result)
+    except:
+        print('\n---ERROR---\nInvalid Input')
+
+emoji_math()
