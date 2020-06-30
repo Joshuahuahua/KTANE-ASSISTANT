4198567361eb204b0441f8a1464636ab5fdd114f
Joshuahuahua <joshuahollander2001@gmail.com>
2020-06-30 15:53:39 +0800

    Update colour_flash.py

--- modules/modded/colour_flash.py
+++ modules/modded/colour_flash.py
@@ -1,29 +1,53 @@
 def colour_flash(bomb_data):
     colour_dic = ['red', 'yellow', 'green', 'blue', 'magenta', 'white']
-    
+    '''
     word_input = input('Usage <word1, word2, etc>: ').lower().replace(', ', ',').split(',')
     colour_input = input('Usage <colour1>, colour2, etc>: ').lower().replace(', ', ',').split(',')
-    
+
     convert(word_input, colour_dic)
     convert(colour_input, colour_dic)
 
     if len(word_input) != 8 or len(colour_input) != 8:
         print('\n--ERROR--\nExpected 8 items!')
         return
+    '''
 
 
-    if colour_input[-1] == 'red':
-        pass
-    elif colour_input[-1] == 'yellow':
-        pass
-    elif colour_input[-1] == 'green':
-        pass
-    elif colour_input[-1] == 'blue':
-        pass
-    elif colour_input[-1] == 'magenta':
-        pass
-    else:
-        pass
+    word_input = ['red', 'white', 'magenta', 'magenta', 'blue', 'green', 'white', 'white']
+    colour_input = ['white', 'magenta', 'blue', 'blue', 'green', 'yellow', 'magenta', 'red']
+
+    print('Press word:', colour_flash_answer(word_input, colour_input))
+    
+    
+    
+    def colour_flash_answer(word_input, colour_input)
+        if colour_input[-1] == 'red':
+            green_count = 0
+            for i, x in enumerate(word_input):
+                if word_input[i] == 'green' or colour_input[i] == 'green':
+                    green_count+=1
+                    if green_count == 3:
+                        return i
+            if word_input.count('blue') == 1:
+                
+
+
+        elif colour_input[-1] == 'yellow':
+            pass
+        elif colour_input[-1] == 'green':
+            pass
+        elif colour_input[-1] == 'blue':
+            pass
+        elif colour_input[-1] == 'magenta':
+            pass
+        else:
+            pass
+
+
+
+
+
+
 
 
 
@@ -35,4 +59,22 @@ def convert(input_raw, colour_dic):
     
 
 
-colour_flash('w')
+bomb_data = {
+    'bat_AA': 0,
+    'bat_B': 0,
+    'bat_total': 0,
+    'ind_LIT': [],
+    'ind_UNLIT': [],
+    'serial': 'AAAA',
+    'serial_odd': 0,
+    'serial_vowel': 0,
+    'port_parallel': 0,
+    'port_dvi': False,
+    'port_ps2': False,
+    'port_rj45': False,
+    'port_serial': False,
+    'port_rca': False,
+    'modules_total': 101,
+    'modules_solved': 0,
+}
+colour_flash(bomb_data)
