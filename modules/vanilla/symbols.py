039d1d84fb4fcf696f302bed98e6ae9aa3960e22
Joshuahuahua <joshuahollander2001@gmail.com>
2020-08-17 00:12:06 +0800

    Updated symbols.py in accordance with the master branch
    This change includes aliases for the symbols and a table of said symbols and their unicode equivalents

--- modules/vanilla/symbols.py
+++ modules/vanilla/symbols.py
@@ -1,39 +1,36 @@
 #Coded by Joshuahuahua
 def symbols():
-    symbols_input = input('Usage <symbol1, symbol2, symbol3, symbol4>: ').lower().replace(', ', ',').replace('6', 'six').split(',')
-    if len(symbols_input) != 4:
-        print('--ERROR--\nExpected 4 items!')
-        return
-    symbols = [
-        {'name': 'paddle', 'character': 'Ϙ', 'value': 1},
-        {'name': 'a', 'character': 'Ѧ', 'value': 2},
-        {'name': 'landa', 'character': 'ƛ', 'value': 3},
-        {'name': 'lightning', 'character': 'Ϟ', 'value': 4},
-        {'name': 'alien', 'character': 'Ѭ', 'value': 5},
-        {'name': 'curly h', 'character': 'ϗ', 'value': 6},
-        {'name': 'backwards c', 'character': 'Ͽ', 'value': 7},
-        {'name': 'e', 'character': 'Ӭ', 'value': 8},
-        {'name': 'squiggle', 'character': 'Ҩ', 'value': 9},
-        {'name': 'white star', 'character': '☆', 'value': 10},
-        {'name': '?', 'character': '¿', 'value': 11},
-        {'name': 'copyright', 'character': '©', 'value': 12},
-        {'name': 'butt', 'character': 'Ѽ', 'value': 13},
-        {'name': 'x', 'character': 'Җ', 'value': 14},
-        {'name': 'r', 'character': 'Ԇ', 'value': 15},
-        {'name': 'six', 'character': 'Ϭ', 'value': 16},
-        {'name': 'p', 'character': '¶', 'value': 17},
-        {'name': 'b', 'character': 'Ѣ', 'value': 18},
-        {'name': 'smiley face', 'character': 'ټ', 'value': 19},
-        {'name': 'trident', 'character': 'ψ', 'value': 20},
-        {'name': 'c', 'character': 'Ͼ', 'value': 21},
-        {'name': 'alien 3', 'character': 'Ѯ', 'value': 22},
-        {'name': 'black star', 'character': '★', 'value': 23},
-        {'name': 'not equal', 'character': '҂', 'value': 24},
-        {'name': 'ae', 'character': 'æ', 'value': 25},
-        {'name': 'straight h', 'character': 'Ҋ', 'value': 26},
-        {'name': 'omega', 'character': 'Ω', 'value': 27},
-    ]
     
+    symbols = [
+        {'name': ['paddle', 'hoop'], 'character': 'Ϙ', 'value': 1},
+        {'name': ['a', 'pyramid'], 'character': 'Ѧ', 'value': 2},
+        {'name': ['lambda', 'half life', 'halflife'], 'character': 'ƛ', 'value': 3},
+        {'name': ['lightning', 'lightning bolt', 'bolt'], 'character': 'Ϟ', 'value': 4},
+        {'name': ['alien', 'train'], 'character': 'Ѭ', 'value': 5},
+        {'name': ['curly h', 'h'], 'character': 'ϗ', 'value': 6},
+        {'name': ['backwards c'], 'character': 'Ͽ', 'value': 7},
+        {'name': ['e', 'backwards e'], 'character': 'Ӭ', 'value': 8},
+        {'name': ['curl', 'squiggle'], 'character': 'Ҩ', 'value': 9},
+        {'name': ['line star', 'white star'], 'character': '☆', 'value': 10},
+        {'name': ['?', 'question mark', 'questionmark'], 'character': '¿', 'value': 11},
+        {'name': ['copyright'], 'character': '©', 'value': 12},
+        {'name': ['butt', 'w'], 'character': 'Ѽ', 'value': 13},
+        {'name': ['x', 'xi', 'ix', 'i'], 'character': 'Җ', 'value': 14},
+        {'name': ['r', 'broken 3', 'hook'], 'character': 'Ԇ', 'value': 15},
+        {'name': ['six', 'g', 'upsidedown g'], 'character': 'Ϭ', 'value': 16},
+        {'name': ['p', 'paragraph', 'backwards p'], 'character': '¶', 'value': 17},
+        {'name': ['b', 'tb', 'bt'], 'character': 'Ѣ', 'value': 18},
+        {'name': ['smile', 'smiley face', 'smileyface', ':p'], 'character': 'ټ', 'value': 19},
+        {'name': ['trident', 'fork'], 'character': 'ψ', 'value': 20},
+        {'name': ['c'], 'character': 'Ͼ', 'value': 21},
+        {'name': ['snake', 'alien 3', 'worm', 'three'], 'character': 'Ѯ', 'value': 22},
+        {'name': ['filled star', 'black star'], 'character': '★', 'value': 23},
+        {'name': ['not equal', 'train track', 'dumbbell'], 'character': '҂', 'value': 24},
+        {'name': ['ae'], 'character': 'æ', 'value': 25},
+        {'name': ['n', 'straight h', 'backwards n'], 'character': 'Ҋ', 'value': 26},
+        {'name': ['omega'], 'character': 'Ω', 'value': 27}
+    ]
+
     column_list = [
         {'column': [1,2,3,4,5,6,7], 'total': 0},
         {'column': [8,1,7,9,10,6,11], 'total': 0},
@@ -44,11 +41,25 @@ def symbols():
     ]
 
     column_found = 0
+
+    print('============================')
+    for symbol in symbols:
+        sym_name = symbol['name'][0]
+        sym_char = symbol['character']
+        print(sym_name.rjust(13) + ' | ' + sym_char)
+    print('============================')
+
+    raw_input = input('Usage <symbol1, symbol2, symbol3, symbol4>: ').lower().replace(', ', ',').replace('6', 'six').replace('3', 'three').split(',')
+    symbols_input = raw_input[:]
+    if len(symbols_input) != 4:
+        print('--ERROR--\nExpected 4 items!')
+        return
     
+
     # Convert name to value
     for i, current_input in enumerate(symbols_input):
         for symbol in symbols:
-            if current_input == symbol['name']:
+            if current_input in symbol['name']:
                 symbols_input[i] = symbol['value']
     try:
         sum(symbols_input)
@@ -72,5 +83,7 @@ def symbols():
     print('\n---ORDER---')
     for symbol in column_list[column_found-1]['column']:
         if symbol in symbols_input:
-            print(str(symbols[symbol-1]['name']))
+            for alias in symbols[symbol-1]['name']:
+                if alias in raw_input:
+                    print(alias)
     print('-----------')
